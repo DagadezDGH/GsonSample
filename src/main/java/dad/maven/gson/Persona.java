4bf40f938510f033034cb9ea917d6e43c26a9b08
DagadezDGH <davidgarridohdez@gmail.com>
2020-10-02 19:57:36 +0100

    Initial commit

---
+++ src/main/java/dad/maven/gson/Persona.java
@@ -0,0 +1,26 @@
+package dad.maven.gson;
+
+public class Persona {
+	 String Nombre;
+	 String Apellido;
+	 int Edad;
+	public void setfirstName(String nombre) {
+		// TODO Auto-generated method stub
+		Nombre=nombre;
+		
+		
+	}
+
+	public void setlastName(String apellido) {
+		// TODO Auto-generated method stub
+		Apellido=apellido;
+	}
+
+	public void setage(int edad) {
+		// TODO Auto-generated method stub
+		Edad=edad;
+	}
+	public Persona() {
+		
+	}
+}
